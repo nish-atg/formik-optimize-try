83759ba9fd190d6baca6af0109227c3b4fe16673
nishkohli96 <nishkohli96@gmail.com>
2022-08-08 17:02:28 +0530

    fieldArray

--- src/views/Performant/index.tsx
+++ src/views/Performant/index.tsx
@@ -1,31 +1,103 @@
-import { Grid } from "@mui/material";
-import { Formik, Form } from "formik";
-import React, { memo } from "react";
-import { PerformantTextField } from "../../components/Fields/Form/PerformantTextField";
+import {Grid, Select, MenuItem} from '@mui/material';
+import {Formik, Form, FieldArray} from 'formik';
+import * as yup from 'yup';
+import React, {memo} from 'react';
+import {PerformantTextField} from '../../components/Fields/Form/PerformantTextField';
 
 export type PerformantProps = {};
 
+const options = ['ford', 'ferrari', 'hyundai', 'toyota'];
+
+const Schema = yup.object().shape({
+  loadItems: yup.array().of(
+    yup.object().shape({
+      someText: yup
+        .string()
+        .trim()
+        .ensure()
+        .min(3, 'Please describe with minimum 3 characters')
+        .required('rquired'),
+      brand: yup
+        .string()
+        .oneOf(options, 'Please select a brand.')
+        .ensure()
+        .required('required'),
+      description: yup
+        .string()
+        .trim()
+        .ensure()
+        .min(1, 'Please describe with minimum 3 characters')
+        .required('rquired'),
+    })
+  ),
+});
+
 export const Performant: React.FC<PerformantProps> = memo((props) => {
-  const totalFields = new Array<number>(30).fill(0);
+  const totalFields = new Array(100).fill({someText: '', brand: ''});
   return (
     <Formik
       initialValues={{
-        state: {},
+        loadItems: totalFields,
       }}
+      validationSchema={Schema}
+      validateOnChange={false}
       onSubmit={() => {}}
     >
-      <Form>
-        <Grid container={true} spacing={3}>
-          {totalFields.map((f, i) => (
-            <Grid item={true} xs={12} md={4} key={`state.field-${i}`}>
-              <PerformantTextField
-                label={"Field #" + (i + 1)}
-                name={`state.field-${i}`}
-              />
+      {({values}) => {
+        return (
+          <Form>
+            <Grid container style={{padding: 10}}>
+              <FieldArray name='loadItems'>
+                {(arrayHelpers: any) => (
+                  <div>
+                    {values.loadItems.map((f, i) => (
+                      <Grid
+                        container
+                        key={`field-${i}`}
+                        style={{marginBottom: 10}}
+                      >
+                        <Grid
+                          container
+                          item
+                          xs={1}
+                          justifyContent='center'
+                          alignItems='center'
+                        >
+                          <span>{i + 1}</span>
+                        </Grid>
+                        <Grid item xs={3}>
+                          <PerformantTextField
+                            label={'someText #' + (i + 1)}
+                            name={`loadItems[${i}].someText`}
+                          />
+                        </Grid>
+                        <Grid item xs={5}>
+                          <Select
+                            name={`loadItems[${i}].brand`}
+                            style={{width: 150, marginLeft: 20}}
+                          >
+                            {options.map((option, index) => (
+                              <MenuItem key={index} value={option}>
+                                {option}
+                              </MenuItem>
+                            ))}
+                          </Select>
+                        </Grid>
+                        <Grid item xs={3}>
+                          <PerformantTextField
+                            label={'Description #' + (i + 1)}
+                            name={`loadItems[${i}].description`}
+                          />
+                        </Grid>
+                      </Grid>                
+                    ))}
+                  </div>
+                )}
+              </FieldArray>
             </Grid>
-          ))}
-        </Grid>
-      </Form>
+          </Form>
+        );
+      }}
     </Formik>
   );
 });
--- src/views/Root/index.tsx
+++ src/views/Root/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Home } from "../Home";
 import { Performant } from "../Performant";
 import { Problematic } from "../Problematic/";
+import {PerformantOld} from '../Performant-old';
 
 export type RootProps = {};
 
@@ -10,7 +11,10 @@ export const Root: React.FC<RootProps> = memo((props) => {
   return (
     <>
       <BrowserRouter>
-        <Link to={"/performant"}>Go To Performant View</Link>
+        <Link to={"/performant"}>Go To Performant View with Yup Validation (test this)</Link>
+        <br/>
+        <br/>
+        <Link to={"/performant-old"}>Original Performant Component</Link>
         <br />
         <br />
         <Link to={"/problematic"}>Go To Problematic View</Link>
@@ -19,6 +23,7 @@ export const Root: React.FC<RootProps> = memo((props) => {
         <Routes>
           <Route path="/problematic" element={<Problematic />} />
           <Route path="/performant" element={<Performant />} />
+          <Route path="/performant-old" element={<PerformantOld />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
